e8e8fc5e15b8b779ecfb0c46a6bcaa0b333eb83b
Sahin Bosnic <sahin.bosnic@hotmail.com>
2017-03-12 16:59:20 +0100

    Setting up angular

---
+++ JourneyApp/JourneyApp/Scripts/app.js
@@ -0,0 +1,40 @@
+﻿angular.module("journeyApp", ['ngRoute']);
+
+angular.module("journeyApp").config(
+
+    function ($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl: "/templates/routing/login.html",
+            controller: 'loginController'
+        })
+        .when("/journey", {
+            templateUrl: "/templates/routing/journey.html",
+            controller: 'journeyController'
+        })
+        .when("/start", {
+            templateUrl: "/templates/routing/start.html",
+            controller: 'startController'
+        })
+        .when("/stop", {
+                templateUrl: "/templates/routing/stop.html",
+                controller: 'stopController'
+            })
+        .when("/404", {
+            templateUrl: "/templates/routing/404.html",
+            controller: '404'
+        })
+        .otherwise({
+            redirectTo: "/404"
+        });
+    }).controller('loginController', function ($scope) {
+
+    }).controller('startController', function ($scope) {
+        console.log(new Date());
+        //$scope.currentDate = new Date();
+
+    }).controller('stopController', function ($scope) {
+
+    }).controller('journeyController', function ($scope) {
+
+    });
