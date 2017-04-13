21af3e0436e7deab97703f00cdb8e30e05342a98
Sahin Bosnic <sahin.bosnic@hotmail.com>
2017-04-13 16:04:28 +0200

    Got API to work

---
+++ JourneyApp/JourneyWeb/API/VehicleController.cs
@@ -0,0 +1,33 @@
+﻿using JourneyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+//using System.Web.Mvc;
+
+namespace JourneyWeb.Controllers
+{
+    public class VehicleController : ApiController
+    {
+        //private DefaultDataContext db = new DefaultDataContext();
+        List<Vehicle> _vehicles = new List<Vehicle>()
+        {
+            new Vehicle { Id = 1, User = null, NumberPlate = "xem225", Active = true, Odometer = 276544, Note = null },
+            new Vehicle { Id = 2, User = null, NumberPlate = "fke992", Active = true, Odometer = 276544, Note = null },
+            new Vehicle { Id = 3, User = null, NumberPlate = "ljf541", Active = true, Odometer = 276544, Note = null }
+        };
+
+        [HttpGet]
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            return _vehicles;
+        }
+
+        /*/ GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
--- JourneyApp/JourneyWeb/App_Start/WebApiConfig.cs
+++ JourneyApp/JourneyWeb/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace JourneyWeb
 {
@@ -10,6 +11,9 @@ namespace JourneyWeb
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            //Transform response into JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
