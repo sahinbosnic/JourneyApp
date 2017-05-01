8be1a2178b5bb49a14ba946ace1f5de09e37360f
Sahin Bosnic <sahin.bosnic@hotmail.com>
2017-05-02 01:35:47 +0200

    Less

--- JourneyApp/JourneyWeb/Controllers/JourneyController.cs
+++ JourneyApp/JourneyWeb/Controllers/JourneyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,14 @@ namespace JourneyWeb.Controllers
 {
     public class JourneyController : Controller
     {
+        ILog log = log4net.LogManager.GetLogger(typeof(HomeController));
         // GET: Journey
         public ActionResult Index()
         {
+            log.Debug("Debug message");
+            log.Warn("Warn message");
+            log.Error("Error message");
+            log.Fatal("Fatal message");
             return View();
         }
     }
